891e47d1e4a1fa8407cb62a06bb75938f96f193f
Paul Abakada <33938655+paulynson@users.noreply.github.com>
2021-06-10 18:12:39 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+let msg = document.querySelector(".msg");
+
+// fetch("http://numbersapi.com/1")
+// .then(response => response.text())
+// .then(data => { 
+//   msg.innerHTML = data;
+// })     
+// .catch(err => err.message());
+
+
+// let xhr = new XMLHttpRequest();
+
+// xhr.onreadystatechange = function(){
+// if(xhr.readyState == 4 && xhr.status == 200){
+// msg.innerHTML = xhr.responseText;
+// msg.style.color = "#ffffff";
+// msg.style.backgroundColor = "teal";
+// msg.style.padding = "50px";
+// }
+// else {
+// let err = "Data cannot be fatched"
+// msg.innerHTML = err;
+// msg.style.color = "#ffffff";
+// msg.style.backgroundColor = "red";
+// msg.style.padding = "50px";
+// }
+
+// }
+
+// xhr.open("GET", "http://numbersapi.com/21", true);
+// xhr.send();
